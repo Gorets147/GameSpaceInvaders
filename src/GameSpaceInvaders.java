8e5a5bb5ed6a01bb29a7b93e2348be0f74154440
Gorets147 <mih.kom1998@gmail.com>
2023-03-06 16:19:32 +0300

    add GameSpaceInvaders class and workspace.xml

--- src/GameSpaceInvaders.java
+++ src/GameSpaceInvaders.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class GameSpaceInvaders {
+import javax.swing.*;
+import java.awt.*;
+
+class GameSpaceInvaders extends JFrame{
+
+    public static void main(String[] args) {
+        new GameSpaceInvaders().go();
+    }
+
+    GameSpaceInvaders {
+
+    }
+
+    void go() {
+
+    }
+
+    class Cannon {
+
+    }
+
+    class Ray {
+
+    }
+
+    class Alien {
+
+    }
+
+    class Wave{
+
+    }
+
+    class Canvas extends JPanel {
+        @Override
+        public void paint(Graphics g) {
+            super.paint(g);
+        }
+    }
 }
